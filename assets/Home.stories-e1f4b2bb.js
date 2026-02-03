cb7685292df644db380c56a4133e4f96ef3f42d6
3asoftwares <3asoftwares@users.noreply.github.com>
2026-02-03 12:57:20 +0000

    deploy: 8603def14e179def006df27830f35f65523b0a7c

---
+++ assets/Home.stories-e1f4b2bb.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-bb4eca5c.js";import"./index-9ab4f09c.js";import"./_commonjsHelpers-725317a4.js";const p={title:"Home",parameters:{layout:"centered",docs:{page:null}}},e=()=>o.jsxs("div",{style:{textAlign:"center",marginTop:40},children:[o.jsx("h1",{children:"Welcome to the 3aSoftwares UI Storybook."}),o.jsxs("p",{style:{fontSize:18,margin:"20px 0"},children:["Explore the available UI components using the sidebar.",o.jsx("br",{}),"Select a component to see its stories, props, and usage examples."]})]});e.__docgenInfo={description:"",methods:[],displayName:"Welcome"};var t,s,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`() => <div style={{
+  textAlign: 'center',
+  marginTop: 40
+}}>
+    <h1>Welcome to the 3aSoftwares UI Storybook.</h1>
+    <p style={{
+    fontSize: 18,
+    margin: '20px 0'
+  }}>
+      Explore the available UI components using the sidebar.
+      <br />
+      Select a component to see its stories, props, and usage examples.
+    </p>
+  </div>`,...(n=(s=e.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const l=["Welcome"];export{e as Welcome,l as __namedExportsOrder,p as default};
